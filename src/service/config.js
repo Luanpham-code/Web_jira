a880fc8214b823101fe8f985aadc03bd79aeff22
Luampham-code <luan.phamthanh0405@gmail.com>
2025-10-08 06:19:51 +0700

    luan3

--- src/service/config.js
+++ src/service/config.js
@@ -5,7 +5,7 @@ export const axiosCustom = axios.create({
   headers: {
     "Content-Type": "application/json",
     TokenCybersoft:
-      "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJjeWJlcmxlYXJuQGdtYWlsLmNvbSIsIm5iZiI6MTc1OTcwMjEwOSwiZXhwIjoxNzU5NzA1NzA5fQ.QVKf5yFMTekwahqBsFXEWT0mW0zPQjB-nBKDTdmoTAY",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA4NSIsIkhldEhhblN0cmluZyI6IjExLzAyLzIwMjYiLCJIZXRIYW5UaW1lIjoiMTc3MDc2ODAwMDAwMCIsIm5iZiI6MTc0MzAxMjAwMCwiZXhwIjoxNzcwOTE5MjAwfQ._5a1o_PuNL8CuHuGdsi1TABKYJwuMsnG5uSKAILfaY8",
   },
 });
 
